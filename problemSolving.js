67ea5ed1b13c2adb01c77dfd6340f2fd5ebc3cd5
mursalin@gmail.com <MURSA>
2023-07-22 12:11:38 +0600

    solved the problem number 1

---
+++ problemSolving.js
@@ -0,0 +1,11 @@
+//|| problem number 1 : Write a function to find the sum of two numbers.||
+// first making the simple sum function ...
+function sumTwoNumbers(a, b) {
+  return a + b;
+}
+const number1 = 5;
+const number2 = 10;
+const result = sumTwoNumbers(number1, number2);
+console.log(result); //result output : 15
+
+//|| problem number 2 : Create a function to check if a given number is even or odd ||
